1b5386a845e1a9a130d0b1df78d386832b59b465
platykun <nagonago561@yahoo.co.jp>
2020-05-09 02:06:24 +0900

    api送信用のtsファイル作成

--- lib/api/clips.ts
+++ lib/api/clips.ts
@@ -1,9 +1,15 @@
-import { ListClipsResponse, PostClipRequest, PostClipResponse } from 'holo-back'
+import { ListClipsRequest, ListClipsResponse, PostClipRequest, PostClipResponse } from 'holo-back'
 import api from './helper/apiRequestHelper'
 
 export default class ClipsApiTs {
   static get (): Promise<ListClipsResponse> {
-    return api.get<null, ListClipsResponse>('/clips', null)
+    const url = '/clips'
+    return api.get<null, ListClipsResponse>(url, null)
+  }
+
+  static getList (getClipRequest: ListClipsRequest): Promise<ListClipsResponse> {
+    const url = '/clips'
+    return api.get<ListClipsRequest, ListClipsResponse>(url, getClipRequest)
   }
 
   static post (postClipRequest: PostClipRequest): Promise<PostClipResponse> {
